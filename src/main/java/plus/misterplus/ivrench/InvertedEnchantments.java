6fdb140999cc3d807e7552d537f9d4d08cee9c6b
misterplus <whiteplayz45@gmail.com>
2020-03-30 09:46:15 +0800

    Version 1.0.3

--- src/main/java/plus/misterplus/ivrench/InvertedEnchantments.java
+++ src/main/java/plus/misterplus/ivrench/InvertedEnchantments.java
@@ -9,7 +9,7 @@ import org.apache.logging.log4j.Logger;
 public class InvertedEnchantments {
     public static final String MOD_ID = "ivrench";
     public static final String MOD_NAME = "Inverted Enchantments";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
 
     private static Logger logger = LogManager.getLogger(MOD_NAME);
 
